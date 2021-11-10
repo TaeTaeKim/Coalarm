aa9b392ed4ef3972411a2cd3b628bcae441f3421
TaeTaeKim <hc07car@gmail.com>
2021-11-10 14:53:56 +0900

    SQL script

--- sqlScript/Coalam.sql
+++ sqlScript/Coalam.sql
@@ -1,16 +1,14 @@
 CREATE TABLE country(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     isocode VARCHAR(10) NOT NULL,
     countryName VARCHAR(50) NOT NULL,
     caution_lvl INT(1) NOT NULL
-    PRIMARY KEY(id)
 );
 
 CREATE TABLE notice(
-    id INT NOT NULL AUTO_INCREMENT,
-    isocode VARCHAR(10) NOT NULL,
-    notice TEXT NOT NULL
-
-    PRIMARY KEY(id)
-    FOREIGN KEY(isocode) REFERENCES country(isocode) ON UPDATE CASCADE
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    country_isocode VARCHAR(10) NOT NULL,
+    notice TEXT NOT NULL,
+    FOREIGN KEY(country_isocode) REFERENCES country(isocode) 
+    ON UPDATE CASCADE ON DELETE CASCADE
 );
