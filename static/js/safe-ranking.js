492e4894e1768bcaf9ab1660f2cb81f40bdb1c41
용석 박 <4up456@naver.com>
2021-11-11 21:51:02 +0900

    link수정

--- static/js/safe-ranking.js
+++ static/js/safe-ranking.js
@@ -3,6 +3,7 @@ let rankingEl = $('.ranking-body');
 
 data = [
   {
+    link: "#",
     flag: '../static/img/한국.jpg',
     country: '한국',
     point: 90,
@@ -10,6 +11,7 @@ data = [
     continent: 'East Asia'
   },
   {
+    link: "#",
     flag: '../static/img/미국.png',
     country: '미국',
     point: 80,
@@ -17,6 +19,7 @@ data = [
     continent: 'America'
   },
   {
+    link: "#",
     flag: '../static/img/싱가폴.png',
     country: '싱가폴',
     point: 70,
@@ -27,7 +30,7 @@ data = [
 
 data.forEach((el, i)=>{
   $(rankingEl).append(
-    `<div class="ranking-box">
+    `<a href=${el.link} class="ranking-box">
         <img src=${el.flag} alt="국기사진${i+1}">
         <div class="ranking-itmes">
             <div class="star"><i class="fa fa-star"></i> ${i+1}위</div>
@@ -39,6 +42,6 @@ data.forEach((el, i)=>{
             <div class="period"><i class="fa fa-clock"></i> 자가격리 기간: ${el.isolation}일</div>
             <div class="continent"><i class="fa fa-globe"></i> 대륙: ${el.continent}</div>
         </div>
-    </div>`
+    </a>`
   );
 })
