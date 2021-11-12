b73f9f56e98f48ad61736cad0486c465519f60da
TaeTaeKim <hc07car@gmail.com>
2021-11-12 19:04:13 +0900

    test

--- static/js/map.js
+++ static/js/map.js
@@ -1,19 +1,51 @@
 let Zoom = 'world';
+const countryCode = [
+  //전세카이
+  {name: '.world', code: 'world'},
+  //아프리카
+  {name: '.Africa', code: '002'},
+  {name: '.northernAfrica', code: '015'},
+  {name: '.westernAfrica', code: '011'},
+  {name: '.middleAfrica', code: '017'},
+  {name: '.easternAfrica', code: '014'},
+  {name: '.southernAfrica', code: '018'},
+  //유럽
+  {name: '.Europe', code: '150'},
+  {name: '.northernEurope', code: '154'},
+  {name: '.westernEurope', code: '155'},
+  {name: '.easternEurope', code: '151'},
+  {name: '.southernEurope', code: '039'},
+  //미대륙
+  {name: '.Americas', code: '019'},
+  {name: '.northernAmericas', code: '021'},
+  {name: '.caribbean', code: '029'},
+  {name: '.centralAmericas', code: '013'},
+  {name: '.southAmericas', code: '005'},
+  //아시아
+  {name: '.Asia', code: '142'},
+  {name: '.centralAsia', code: '143'},
+  {name: '.easternAsia', code: '030'},
+  {name: '.southernAsia', code: '034'},
+  {name: '.southEasternAsia', code: '035'},
+  {name: '.westernAsia', code: '145'},
+  //오세아니아
+  {name: '.Oceania', code: '009'},
+  {name: '.australiaAndNewZealand', code: '053'},
+  {name: '.melanesia', code: '054'},
+  {name: '.micronesia', code: '057'},
+  {name: '.polynesia', code: '061'}
+];
+
+// 지도 소분류 클릭 이동하기
+countryCode.forEach((el) => {
+  $(el.name).on('click', function () {
+    Zoom = el.code;
+    rendermap();
+  })
+});
+
 // 페이지 resize시에 지도 갱신
 $(window).resize(function(){location.reload();});
-$(document).ready(function(){
-  $.ajax({
-    type:"GET",
-    url:"/",
-    dataType:'json',
-    success:function(response){
-      if (response['result']=='success'){
-        let caution_lvl = response['caution']
-        console.log(caution_lvl);
-      }
-    }
-  })
-})
 
 //dropmenu관련 설정
 $(document).ready(()=>{
@@ -26,6 +58,21 @@ $(document).ready(()=>{
   })
 
 })
+
+
+
+
+
+$.ajax({
+  type:"GET",
+  url:"/",
+  dataType:'json',
+  success:function(response){
+      console.log(response.data);
+      alert('hello')
+    }
+  })
+
 
 
 //map rendering하는 함수
@@ -82,49 +129,7 @@ function rendermap() {
     chart.draw(data, options);
   }
 }
-const countryCode = [
-  //전세카이
-  {name: '.world', code: 'world'},
-  //아프리카
-  {name: '.Africa', code: '002'},
-  {name: '.northernAfrica', code: '015'},
-  {name: '.westernAfrica', code: '011'},
-  {name: '.middleAfrica', code: '017'},
-  {name: '.easternAfrica', code: '014'},
-  {name: '.southernAfrica', code: '018'},
-  //유럽
-  {name: '.Europe', code: '150'},
-  {name: '.northernEurope', code: '154'},
-  {name: '.westernEurope', code: '155'},
-  {name: '.easternEurope', code: '151'},
-  {name: '.southernEurope', code: '039'},
-  //미대륙
-  {name: '.Americas', code: '019'},
-  {name: '.northernAmericas', code: '021'},
-  {name: '.caribbean', code: '029'},
-  {name: '.centralAmericas', code: '013'},
-  {name: '.southAmericas', code: '005'},
-  //아시아
-  {name: '.Asia', code: '142'},
-  {name: '.centralAsia', code: '143'},
-  {name: '.easternAsia', code: '030'},
-  {name: '.southernAsia', code: '034'},
-  {name: '.southEasternAsia', code: '035'},
-  {name: '.westernAsia', code: '145'},
-  //오세아니아
-  {name: '.Oceania', code: '009'},
-  {name: '.australiaAndNewZealand', code: '053'},
-  {name: '.melanesia', code: '054'},
-  {name: '.micronesia', code: '057'},
-  {name: '.polynesia', code: '061'}
-];
 
-countryCode.forEach((el) => {
-  $(el.name).on('click', function () {
-    Zoom = el.code;
-    rendermap();
-  })
-});
 
 
 rendermap();
