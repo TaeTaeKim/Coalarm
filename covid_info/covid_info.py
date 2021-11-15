c75fa1ecc43095b1e412b2f9f64e60a6b1446ca3
유승아 <aaay8228@gmail.com>
2021-11-15 17:44:00 +0900

    covid_info

--- covid_info/covid_info.py
+++ covid_info/covid_info.py
@@ -3,19 +3,20 @@ import time
 import re
 import json
 
-dict = []  # 국가별 코로나 정보를 저장할 딕셔너러
+dict = []  # 국가별 코로나 정보를 저장할 리스트
 
 # 웹사이트 실행
 with webdriver.Chrome() as driver:
     driver.get('https://coronaboard.kr/en/')
 
-    # 더보기 버튼 누르기 (스크롤 후 버튼 클릭)
+    # 더보기 버튼 누르기 (버튼 클릭)
     for i in range(2):
         time.sleep(5)
         try:
             some_element = driver.find_element_by_id('show-more')
             some_element.click()
-        except: print("시간이 부족합니다.")                            # time.sleep 적절한 값 찾기
+        except: print("시간이 부족합니다.")    # time.sleep 적절한 값 찾기
+
     # table 읽기
     table = driver.find_element_by_tag_name('tbody')
     tr = table.find_elements_by_tag_name('tr')
@@ -39,13 +40,13 @@ with webdriver.Chrome() as driver:
             c = "N/A"       # new_deaths, total_recovered
             d = data[2]     # new_recovered, fatality, recovery, incidence
 
+
         # 데이터 컬럼별로 나누기 (a)
         try:
             country_name = re.search('[A-Za-z]+[\s]?[A-Za-z]+', a).group()
             total_confirmed = re.search('[\d+,?]+\d{3}$', a).group()
         except:
-            total_confirmed = a.split(' ')[-1]              # 정규식으로 걸러지지 않은 값
-
+            total_confirmed = a.split(' ')[-1]   # 정규식으로 걸러지지 않은 값
 
         # 데이터 컬럼별로 나누기 (b와 c)
         if b.startswith('('):
@@ -68,8 +69,7 @@ with webdriver.Chrome() as driver:
             new_deaths, new_recovered, fatality, recovery, incidence = d.split()
 
         # 데이터 형변환 및 결측값 처리
-
-        try: total_confirmed = int(total_confirmed.replace(",", ""))  # , 제거 후 int로 변환
+        try: total_confirmed = int(total_confirmed.replace(',', ''))  # , 제거 후 int로 변환
         except: total_confirmed = -1
 
         try: new_confirmed = int(new_confirmed[2:-1].replace(',', ''))  # new_confirmed값이 있는경우 (+)삭제하고 int 변환
@@ -96,7 +96,6 @@ with webdriver.Chrome() as driver:
         try: incidence = int(incidence.replace(',',''))
         except: incidence = -1
 
-
         data = {
             'country_name' : country_name,
             'total_confirmed' : total_confirmed,
@@ -109,8 +108,7 @@ with webdriver.Chrome() as driver:
             'recovery' : recovery,
             'incidence': incidence
         }
-
         dict.append(data)
-
+        
     with open ('./covid_info.json', 'w') as f:
         json.dump(dict, f)
