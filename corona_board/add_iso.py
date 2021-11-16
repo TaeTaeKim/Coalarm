c1adc619d6805f6b3e094a8642f4c21c2243fa7d
유승아 <aaay8228@gmail.com>
2021-11-16 17:02:41 +0900

    add_iso_code and corona_board

---
+++ corona_board/add_iso.py
@@ -0,0 +1,20 @@
+import json
+
+with open("./country_ISO.json", "r") as f:
+    add_country = json.load(f)
+
+    add_country.append({'Code': 'KR', 'Name': 'South Korea'})
+    add_country.append({'Code': 'XK', 'Name': 'Kosovo'})
+    add_country.append({'Code': 'RE', 'Name': 'R\u00e9union'})    # <----- R
+    add_country.append({'Code': 'TL', 'Name': 'East Timor'})
+    add_country.append({'Code': 'CW', 'Name': 'Cura\u00e7ao'})   # <----Curaçao
+
+    add_country.append({'Code': 'CG', 'Name': 'Democratic Republic of Congo'})
+    add_country.append({'Code': 'BQ', 'Name': 'Bonaire Sint Eustatius and Saba'})
+    add_country.append({'Code': 'CI', 'Name': "Cote d'Ivoire"})
+    add_country.append({'Code': 'CW', 'Name': 'Curacao'})
+    add_country.append({'Code': 'TL', 'Name': 'Timor})
+
+
+with open('./add_country_ISO.json', 'w') as f:
+    json.dump(add_country, f)
