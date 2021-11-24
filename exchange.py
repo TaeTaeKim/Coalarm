1f07aed52513c9c4d5da4083a9e68bb9dac55cdf
정 준호 <blacktick@naver.com>
2021-11-24 09:25:03 +0900

    exchange read api -> json

--- exchange.py
+++ exchange.py
@@ -1,22 +1,24 @@
 # 환율 API를 이용해서 반환하는 함수.
 import requests
+import json
 def exchange(ISO):
-    url  = 'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON'
-    params = {
-        'authkey': 'y6VrwVGHQWHtS19rOIvBg7SJTi1O115y',
-        'data': 'AP01'
-    }
+    # url  = 'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON'
+    # params = {
+    #     'authkey': 'y6VrwVGHQWHtS19rOIvBg7SJTi1O115y',
+    #     'data': 'AP01'
+    # }
     EU = [
         "GG", "JE", "AX", "EE", "FI", "FO", "IE", "IM", "IS", "LT", "LV", "SJ",
         "AT", "BE", "DE", "DD", "FR", "FX", "LI", "LU", "MC", "NL", "BG", "BY", "CZ", "HU", "MD", 
         "PL", "RO", "RU", "SU", "SK", "UA", "AD", "AL", "BA", "ES", "GI", "GR", "HR", "IT", "ME", "MK", 
         "MT", "RS", "PT", "SI", "SM", "VA", "YU"
         ]
-    response = requests.get(url, params=params)
+    # response = requests.get(url, params=params)
     
-    response_text_dict = response.json()
-    
-    dollar = response_text_dict[-1]['deal_bas_r']
+    # response_text_dict = response.json()
+    with open('./static/Test_json/exchange.json','r') as f:
+        response_text_dict = json.load(f)
+    dollar = response_text_dict[4]['deal_bas_r']
 
     if ISO in EU:
         for d in response_text_dict:
@@ -29,4 +31,4 @@ def exchange(ISO):
             exchange_name = i['cur_nm']
             return [exchange_rate,exchange_name]
 
-    return [dollar,response_text_dict[-1]['cur_nm']]
+    return [dollar,response_text_dict[4]['cur_nm']]
