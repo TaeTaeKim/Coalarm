c66a424b919021920359dfed10e9c327de2dc3fa
용석 박 <4up456@naver.com>
2021-11-29 14:25:04 +0900

    메인모바일사이즈

--- static/js/safe-ranking.js
+++ static/js/safe-ranking.js
@@ -1,47 +1,42 @@
-
 let rankingEl = $('.ranking-body');
 
 data = [
   {
-    link: "#",
+    link: '#',
     flag: '../static/img/한국.jpg',
     country: '한국',
     point: 90,
     isolation: 3,
-    continent: '동아시아'
+    continent: '동아시아',
   },
   {
-    link: "#",
+    link: '#',
     flag: '../static/img/미국.png',
     country: '미국',
     point: 80,
     isolation: 7,
-    continent: '미대륙'
+    continent: '미대륙',
   },
   {
-    link: "#",
+    link: '#',
     flag: '../static/img/싱가폴.png',
     country: '싱가폴',
     point: 70,
     isolation: 10,
-    continent: '동아시아'
-  }
-]
+    continent: '동아시아',
+  },
+];
 
-data.forEach((el, i)=>{
+data.forEach((el, i) => {
   $(rankingEl).append(
     `<a href=${el.link} class="ranking-box">
-        <img src=${el.flag} alt="국기사진${i+1}">
+        <img src=${el.flag} alt="국기사진${i + 1}">
         <div class="ranking-itmes">
-            <div class="star"><i class="fa fa-star"></i> ${i+1}위</div>
+            <div class="star"><i class="fa fa-star"></i> ${i + 1}위</div>
             <div class="country-name">${el.country}</div>
             <div class="point">안전점수: <b>${el.point}점</b></div>
         </div>
         <div class="ranking-line"></div>
-        <div class="ranking-info">
-            <div class="period"><i class="fa fa-clock"></i> 자가격리 기간: ${el.isolation}일</div>
-            <div class="continent"><i class="fa fa-globe"></i> 대륙: ${el.continent}</div>
-        </div>
     </a>`
   );
-})
+});
