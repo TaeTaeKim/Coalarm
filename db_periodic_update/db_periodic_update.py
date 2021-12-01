387b777c48dd225a39dff670f100a004377e32fd
최 웅수 <infamousgames@hanmail.net>
2021-12-02 08:39:09 +0900

    asdf

--- db_periodic_update/db_periodic_update.py
+++ db_periodic_update/db_periodic_update.py
@@ -6,13 +6,13 @@ import time
 periodic_update = db_update.DB_Update()
 
 # Api_Data update
-schedule.every().day.at("08:25").do(periodic_update.update_Api_Data)
+schedule.every().day.at("11:00").do(periodic_update.update_Api_Data)
 
 # Vaccine_Data update
 schedule.every().day.at("06:30").do(periodic_update.update_Corona_Vaccine_Data) 
 
 # Corona_Data update
-schedule.every().hour.at(":00").do(periodic_update.update_Corona_Data)
+schedule.every().hour.at(":38").do(periodic_update.update_Corona_Data)
 
 # Embassy_Data update
 schedule.every().day.at("10:30").do(periodic_update.update_Embassy_Data) 
@@ -21,7 +21,7 @@ schedule.every().day.at("10:30").do(periodic_update.update_Embassy_Data)
 schedule.every().monday.do(periodic_update.update_Safety_Data)
 
 # Safety_Score update
-schedule.every().hour.at(":11").do(periodic_update.update_Safety_Score)
+schedule.every().hour.at(":01").do(periodic_update.update_Safety_Score)
 
 
 while True:
